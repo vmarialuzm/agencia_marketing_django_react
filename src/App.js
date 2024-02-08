870bd39af2b5968e93b7e443e5914eb514761d89
vmarialuz <vmarialuzm@gmail.com>
2024-02-08 12:48:27 -0500

    Rutas y redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { Error404 } from "containers/errors/Error404";
+import { Home } from "containers/pages/Home"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import store from './store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="text-gray-500 underline">
-      Hola Mundo
-    </div>
+    <Provider store={store}>
+
+      <Router>
+        <Routes>
+          <Route path="*" element={<Error404 />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+
+    </Provider>
   );
 }
 
